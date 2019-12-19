d0f07274a96da160ebf627c36ec6584061c0dfc7
aliacavdar <ozlemcdevops@gmail.com>
2019-12-18 19:07:20 -0600

    sec_group added

--- output.tf
+++ output.tf
@@ -1,3 +1,11 @@
 output "bucket_name" { 
   value = aws_instance.web.public_up
 } 
+
+output "sec_group" { 
+  value = aws_security_group.allow_ssh.name
+} 
+
+output "sec_group" { 
+  value = aws_key_pair.deployer.key_name
+} 
