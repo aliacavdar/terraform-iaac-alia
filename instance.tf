463fb462af8bb31b6faf90c7bd0db78249a609c7
aliacavdar <ozlemcdevops@gmail.com>
2019-12-18 18:25:02 -0600

    edited instance

--- instance.tf
+++ instance.tf
@@ -1,20 +1,12 @@
 resource "aws_instance" "web" { 
   ami           = "ami-00068cd7555f543d5" 
   instance_type = "t2.micro" 
- 
+  associate_public_ip_address  = "true"
+  key_name = aws_key_pair.deployer.key_name
+  security_groups = ["allow_tls"] 
+  user_data = file("userdata_file")
 
 tags = { 
     Name = "HelloWorld" 
   } 
-} 
- resource "aws_instance" "web" { 
-  ami           = "AMI_ID_HERE" 
-  instance_type = "t2.micro" 
- 
-
- security_groups = ["allow_ssh"] 
-
- tags = { 
-    Name = "HelloWorld" 
-  } 
-} 
+}
