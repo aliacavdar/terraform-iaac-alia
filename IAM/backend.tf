39d41e595cd895297fd38d913ccd9de22122dc53
aliacavdar <ozlemcdevops@gmail.com>
2020-01-04 11:44:40 -0600

    created folders

--- IAM/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "backend-state-alia"
-    key    = "aws/iam/iam_state"
-    region = "us-east-1"
-  }
-}
--- IAM/iam.tf
+++ IAM/iam.tf
@@ -57,4 +57,9 @@ assume_role_policy = <<EOF
 EOF 
 } 
 
- 
+
+ resource "aws_iam_role_policy_attachment" "test-attach" { 
+  role = aws_iam_role.test_role.name 
+  policy_arn = aws_iam_policy.policy.arn 
+} 
+
